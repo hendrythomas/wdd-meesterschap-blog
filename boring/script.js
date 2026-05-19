78a12d53e4878f12daed91e613f64717a579b18b
Thomas Hendry <thomas.hendry@hva.nl>
2026-05-19 13:30:59 +0200

    use script in index

--- boring/script.js
+++ boring/script.js
@@ -1,7 +1,13 @@
-addEventListener('hashchange', (e) => {
+document.addEventListener('hashchange', (e) => {
   // don't scroll down for sections
   const targetSection = document.querySelector('main > section:target');
-  if (targetSection == null) return;
+  if (targetSection === null) return;
 
   window.scrollTo(0, 0);
 });
+
+document.addEventListener('click'), (e) => {
+  // if ('copyId' in e.target.dataset) {
+  //   alert('yay')
+  // }
+}
