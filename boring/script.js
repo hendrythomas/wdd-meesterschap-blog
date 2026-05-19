baaa9a6ea4ae2bb42feead86425e198dc241f09a
Thomas Hendry <thomas.hendry@hva.nl>
2026-05-19 15:06:10 +0200

    add header links

--- boring/script.js
+++ boring/script.js
@@ -1,13 +1,7 @@
-document.addEventListener('hashchange', (e) => {
+addEventListener('hashchange', (e) => {
   // don't scroll down for sections
   const targetSection = document.querySelector('main > section:target');
   if (targetSection === null) return;
 
   window.scrollTo(0, 0);
 });
-
-document.addEventListener('click'), (e) => {
-  // if ('copyId' in e.target.dataset) {
-  //   alert('yay')
-  // }
-}
