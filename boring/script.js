b001d41f9e8b3218a5779bb87f91a28e11eb52c2
Thomas Hendry <thomas.hendry@hva.nl>
2026-05-24 11:24:52 +0200

    add easter egg theme

--- boring/script.js
+++ boring/script.js
@@ -1,11 +1,54 @@
-addEventListener('hashchange', (e) => {
-  // don't scroll down for sections
+document.addEventListener('DOMContentLoaded', () => {
+  setRefreshStyle();
+});
+
+addEventListener('hashchange', () => {
+  preventSectionTargetScroll();
+  highlightActiveLinks();
+  hideEmptyHash();
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Delete') {
+    document.documentElement.classList.remove('styled');
+  }
+});
+
+function setRefreshStyle() {
+  document.documentElement.classList.add('refresh');
+  setTimeout(() => {
+    document.documentElement.classList.remove('refresh');
+  }, 1000);
+}
+
+function preventSectionTargetScroll() {
+  if (!document.documentElement.classList.contains('styled')) return;
+
   const targetSection = document.querySelector('main > section:target');
   if (targetSection === null) return;
 
   window.scrollTo({
     top: 0,
     left: 0,
-    behavior: 'instant',
+    behavior: 'instant'
   });
-});
+}
+
+function highlightActiveLinks() {
+  // remove old highlights
+  const oldActiveLinks = document.querySelectorAll('a.active');
+  for (const activeLink of oldActiveLinks) {
+    activeLink.classList.remove('active');
+  }
+
+  const activeLinks = document.querySelectorAll(`a[href="${location.hash}"]`);
+  for (const activeLink of activeLinks) {
+    activeLink.classList.add('active');
+  }
+}
+
+function hideEmptyHash() {
+  if (location.hash !== '') return;
+
+  history.replaceState({}, '', ' ');
+}
